6bb4504008add33ec66041948bfea996c6038a6f
Drasty4k <barbuta.dragos12@gmail.com>
2021-01-18 16:09:04 +0200

    done

---
+++ 16.Tema pentru 18-01-2021 (ghicitoare)/script.js
@@ -0,0 +1,16 @@
+document.querySelector("#button")
+    .addEventListener("click", function () {
+        nr = 68
+        text = document.querySelector("#text").value
+
+        if (text < nr) {
+            document.querySelector("#result").innerHTML += "Too small"
+        }
+        if (text == nr) {
+            document.querySelector("#result").innerHTML += "You guessed it"
+            
+        }
+        if (text > nr) {
+            document.querySelector("#result").innerHTML += "Too big"
+        }    
+    })
